973104040faf70cb270f1104f6a8fc92344d5e30
SpectrumFATM <tomeastwood338@gmail.com>
2024-09-02 22:18:52 +0100

    Gravity effect added to space.

--- fabric/src/main/java/net/SpectrumFATM/black_archive/fabric/mixin/EntityMixin.java
+++ fabric/src/main/java/net/SpectrumFATM/black_archive/fabric/mixin/EntityMixin.java
@@ -8,18 +8,21 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Random;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
 
+    double oxygenCounter = 0.0;
+
     @Inject(method = "tick", at = @At("HEAD"))
-    public void modifyGravityAndMotion(CallbackInfo ci) {
+    public void tick() {
+        modifyGravityAndMotion();
+        oxygenDepravation();
+    }
+
+    public void modifyGravityAndMotion() {
         Entity entity = (Entity) (Object) this;
         if (!syntheticGravity() && entity.getWorld().getRegistryKey().getValue() == ModDimensions.SPACEDIM_KEY.getValue()) {
 
@@ -61,6 +64,18 @@ public abstract class EntityMixin {
             }
         }
     }
+
+    public void oxygenDepravation() {
+        Entity entity = (Entity) (Object) this;
+
+        if (entity.isAlive()) {
+            oxygenCounter++;
+        }
+
+        if (oxygenCounter > 200) {
+            entity.damage(entity.getWorld().getDamageSources().generic(), 0.5f);
+        }
+     }
 
     //If there is a diamond block within the chunk give the player normal gravity
     private boolean syntheticGravity() {

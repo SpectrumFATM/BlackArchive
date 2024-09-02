820ea9e9519fb9effbdafe2f6413d81da94c4503
SpectrumFATM <tomeastwood338@gmail.com>
2024-09-02 22:19:43 +0100

    Gravity effect added to space.

--- fabric/src/main/java/net/SpectrumFATM/black_archive/fabric/mixin/EntityMixin.java
+++ fabric/src/main/java/net/SpectrumFATM/black_archive/fabric/mixin/EntityMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
@@ -17,7 +18,7 @@ public abstract class EntityMixin {
     double oxygenCounter = 0.0;
 
     @Inject(method = "tick", at = @At("HEAD"))
-    public void tick() {
+    public void tick(CallbackInfo ci) {
         modifyGravityAndMotion();
         oxygenDepravation();
     }

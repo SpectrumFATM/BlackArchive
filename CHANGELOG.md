9227cef373e06135d9ac01e29909001f9ca3f7b2
SpectrumFATM <tomeastwood338@gmail.com>
2025-02-23 20:55:01 +0000

    Fixed buggy animation handler.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,22 +1,4 @@
 ## Changelog
 
 ### Bug Fixes
-- Rani Console on Forge now doesn't crash the game.
-- Fixed TCE on Forge.
-- Fixed Sattenheim.
-- Fixed Oxygen.
-
-### Changed
-- Vortex Manipulator texture.
-- Doric column model.
-
-### Added
-- Added cross-compatibility with Doctor Who Deco and config to match.
-- Added compatibility with Audrey's Additions.
-- Sonic Engine functionality.
-- Silurians.
-- Silurian Hives.
-- Fugitive Interior
-
-### Removed
-- Removed SIDRAT exterior.
+- Flight animations now work in multiplayer.
--- common/src/main/java/net/SpectrumFATM/black_archive/mixin/TardisPilotingMixin.java
+++ common/src/main/java/net/SpectrumFATM/black_archive/mixin/TardisPilotingMixin.java
@@ -2,19 +2,44 @@ package net.SpectrumFATM.black_archive.mixin;
 
 import net.SpectrumFATM.black_archive.world.dimension.ModDimensions;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import whocraft.tardis_refined.common.capability.tardis.TardisLevelOperator;
 import whocraft.tardis_refined.common.tardis.TardisNavLocation;
 import whocraft.tardis_refined.common.tardis.manager.TardisPilotingManager;
 
 @Mixin(TardisPilotingManager.class)
 public abstract class TardisPilotingMixin {
 
+    @Shadow
+    private final TardisLevelOperator operator;
+
+    protected TardisPilotingMixin(TardisLevelOperator operator) {
+        this.operator = operator;
+    }
+
     @Inject(method = "findClosestValidPosition", at = @At("HEAD"), cancellable = true, remap = false)
     public void findClosestValidPosition(TardisNavLocation location, CallbackInfoReturnable<TardisNavLocation> ci) {
         if (location.getDimensionKey().location() == ModDimensions.SPACEDIM_LEVEL_KEY.location() || location.getDimensionKey().location() == ModDimensions.TIMEDIM_LEVEL_KEY.location()) {
             ci.setReturnValue(location);
+        }
+    }
+
+    @Inject(method ="isInFlight", at = @At("HEAD"), remap = false, cancellable = true)
+    public void isInFlight(CallbackInfoReturnable<Boolean> cir) {
+        TardisPilotingManager pilotingManager = (TardisPilotingManager) (Object) this;
+        if (pilotingManager.getCurrentLocation().getDimensionKey().location() == ModDimensions.TIMEDIM_LEVEL_KEY.location() || pilotingManager.getCurrentLocation().getDimensionKey().location() == ModDimensions.TIMEDIM_LEVEL_KEY.location()) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method ="beginFlight", at = @At("HEAD"), remap = false, cancellable = true)
+    private void stopFlightDanceBeforeFlight(boolean autoLand, CallbackInfoReturnable<Boolean> cir) {
+        TardisPilotingManager pilotingManager = (TardisPilotingManager) (Object) this;
+        if (this.operator.getFlightDanceManager().isDancing() && pilotingManager.getCurrentLocation().getDimensionKey().location() == ModDimensions.TIMEDIM_LEVEL_KEY.location() || pilotingManager.getCurrentLocation().getDimensionKey().location() == ModDimensions.TIMEDIM_LEVEL_KEY.location()) {
+            this.operator.getFlightDanceManager().stopDancing();
         }
     }
 }
